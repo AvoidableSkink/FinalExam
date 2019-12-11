56329302e863ef3dce28ce97c7021e7dcf48e630
Kimberly <klindholm7@gmail.com>
2019-12-11 13:57:00 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// FinalExam.cpp : Defines the entry point for the application.
+//
+
+#include "FinalExam.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
